9d6e37365fd538646fa9933ab44c7f9fa031ac30
DESKTOP-QIN6M9H\Ali Haider <syed.ali.haider10@hotmail.com>
2017-10-25 10:54:55 -0700

    Updated live server urls.

--- fypLatest/app/src/main/java/com/sourcey/movnpack/Utility/AppConstants.java
+++ fypLatest/app/src/main/java/com/sourcey/movnpack/Utility/AppConstants.java
@@ -6,11 +6,14 @@ package com.sourcey.movnpack.Utility;
 
 public class AppConstants  {
 
-   // public static String API_SIGNUP = "http://192.168.56.1/FYP_Services/signup.php";
-   // public static String API_LOGIN = "http://192.168.56.1/FYP_Services/local_login1.php";
+    // Local
 
-    public static String API_SIGNUP = "http://192.168.10.20/testfyp/signup.php";
-    public static String API_LOGIN = "http://192.168.10.20/testfyp/local_login1.php";
+    //public static String API_SIGNUP = "http://192.168.10.20/testfyp/signup.php";
+    //public static String API_LOGIN = "http://192.168.10.20/testfyp/local_login1.php";
+
+    // Live
+    public static String API_SIGNUP = "https://androidfyp.000webhostapp.com/signup.php";
+    public static String API_LOGIN = "https://androidfyp.000webhostapp.com/login.php";
 
     public static String SERVICE_PROVIDER = "service_provider";
 
