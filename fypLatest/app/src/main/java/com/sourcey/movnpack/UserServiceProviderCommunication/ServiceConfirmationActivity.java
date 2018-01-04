6f9a221662a1bea67bb958bf1a1a25beaa8cef8b
abdul-nafay <abdulnafaywaseem@gmail.com>
2018-01-05 01:18:37 +0500

    Place Picker Ali ---- Linux

--- fypLatest/app/src/main/java/com/sourcey/movnpack/UserServiceProviderCommunication/ServiceConfirmationActivity.java
+++ fypLatest/app/src/main/java/com/sourcey/movnpack/UserServiceProviderCommunication/ServiceConfirmationActivity.java
@@ -1,6 +1,7 @@
 package com.sourcey.movnpack.UserServiceProviderCommunication;
 
 import android.app.ProgressDialog;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -8,7 +9,11 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.common.GooglePlayServicesNotAvailableException;
+import com.google.android.gms.common.GooglePlayServicesRepairableException;
+import com.google.android.gms.location.places.ui.PlacePicker;
 import com.google.gson.Gson;
 import com.sourcey.movnpack.BidPlacementActivities.UserBidPlacementActivity;
 import com.sourcey.movnpack.DataBase.AcceptedBids;
@@ -49,6 +54,9 @@ public class ServiceConfirmationActivity extends AppCompatActivity implements Me
     AcceptedBidsModel bidResponseToConfirm;
     ConfirmBidModel localBid;
     ProgressDialog progressDialog;
+    int PLACE_PICKER_REQUEST = 1;
+    Place selectedPlace;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -71,7 +79,34 @@ public class ServiceConfirmationActivity extends AppCompatActivity implements Me
 
         doneButton.setOnClickListener(this);
 
+        locationButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
 
+                PlacePicker.IntentBuilder builder = new PlacePicker.IntentBuilder();
+
+                try {
+                    startActivityForResult(builder.build(ServiceConfirmationActivity.this), PLACE_PICKER_REQUEST);
+                } catch (GooglePlayServicesRepairableException e) {
+                    e.printStackTrace();
+                } catch (GooglePlayServicesNotAvailableException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == PLACE_PICKER_REQUEST) {
+            if (resultCode == RESULT_OK) {
+                Place place = PlacePicker.getPlace(data, this);
+                String toastMsg = String.format("Place: %s", place.getName());
+                Toast.makeText(this, toastMsg, Toast.LENGTH_LONG).show();
+            }
+        }
     }
 
     @Override
